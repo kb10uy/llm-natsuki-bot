cc2d77e40e43fd52d0f4e8a514954f25c8c55fb8
Yū KOBAYASHI <kb10uy@kb10uy.org>
2025-04-12 04:40:41 +0900

    各種操作の枠組みだけ実装

--- lnb-server/src/function/reminder.rs
+++ lnb-server/src/function/reminder.rs
@@ -13,18 +13,25 @@ use lnb_core::{
         schema::DescribedSchema,
     },
 };
-use serde::Deserialize;
-use serde_json::json;
-use tracing::info;
+use serde::{Deserialize, Serialize};
+use time::{
+    Date, Duration, OffsetDateTime,
+    format_description::{BorrowedFormatItem, well_known::Rfc3339},
+    macros::format_description,
+};
+use tracing::{info, warn};
+use uuid::Uuid;
+
+const DATE_FORMAT: &[BorrowedFormatItem<'static>] = format_description!("[year]-[month]-[day]");
 
 #[derive(Debug)]
 pub struct Reminder {
-    max_seconds: usize,
+    max_seconds: i64,
 }
 
 #[derive(Debug, Clone, Deserialize)]
 pub struct ReminderConfig {
-    max_seconds: usize,
+    max_seconds: i64,
 }
 
 impl ConfigurableComplexFunction for Reminder {
@@ -45,6 +52,7 @@ impl ComplexFunction for Reminder {
             name: "reminder".to_string(),
             description: r#"
                 ユーザーにリマインダー機能を提供します。
+                - 「いつ」の指定は、基本的にリマインドする内容ではなくリマインドの時刻として解釈してください。
                 - 何時間後や何日後など、確実に期間がわかる場合のみ remind_in にその秒数を指定し、それ以外の場合は remind_at に絶対形式で指定してください。
                 - 現在時刻の情報が必要な場合は local_info で取得し、タイムゾーンは保持してください。
                 - 会話の中でリマインダーのキャンセルを要求された場合、そのリマインダーの設定時のレスポンスに含まれる id を cancel に指定してください。
@@ -100,18 +108,60 @@ impl Reminder {
         _context: &Context,
         parameters: ReminderParameters,
     ) -> Result<FunctionResponse, FunctionError> {
-        info!("{parameters:?}");
+        if let Some(cancel_id) = parameters.cancel {
+            return self.cancel(cancel_id).await;
+        }
+
+        let now = OffsetDateTime::now_local().map_err(FunctionError::by_external)?;
+        let complete_remind_at = if let Some(seconds) = parameters.remind_in {
+            now + Duration::seconds(seconds)
+        } else if let Some(remind_at) = parameters.remind_at {
+            if let Ok(full_datetime) = OffsetDateTime::parse(&remind_at, &Rfc3339) {
+                full_datetime
+            } else if let Ok(date) = Date::parse(&remind_at, DATE_FORMAT) {
+                now.replace_date(date)
+            } else {
+                return self.error(ReminderResponse::InvalidRequest).await;
+            }
+        } else {
+            return self.error(ReminderResponse::InvalidRequest).await;
+        };
+
+        if complete_remind_at - now > Duration::seconds(self.max_seconds) {
+            return self.error(ReminderResponse::DueLimitExceeded).await;
+        }
+
+        self.register(complete_remind_at, parameters.content).await
+    }
+
+    async fn register(&self, remind_at: OffsetDateTime, content: String) -> Result<FunctionResponse, FunctionError> {
+        let id = Uuid::now_v7();
+        let remind_at = remind_at.format(&Rfc3339).map_err(FunctionError::by_serialization)?;
+
+        info!("reminder registered: [{content}] at {remind_at} / {id}");
         Ok(FunctionResponse {
-            result: json!({
-                "status": "accepted",
-                "remind_at": "",
-                "id": "hogehoge",
-            }),
-            /*
-            result: json!({
-                "error": "this feature is not implemented yet",
-            }),
-            */
+            result: serde_json::to_value(ReminderResponse::Registered {
+                id: id.to_string(),
+                remind_at,
+            })
+            .map_err(FunctionError::by_serialization)?,
+            ..Default::default()
+        })
+    }
+
+    async fn cancel(&self, cancel_id: String) -> Result<FunctionResponse, FunctionError> {
+        info!("reminder cancelled: {cancel_id}");
+        Ok(FunctionResponse {
+            result: serde_json::to_value(ReminderResponse::Cancelled { id: cancel_id })
+                .map_err(FunctionError::by_serialization)?,
+            ..Default::default()
+        })
+    }
+
+    async fn error(&self, response: ReminderResponse) -> Result<FunctionResponse, FunctionError> {
+        warn!("reminder error: {response:?}");
+        Ok(FunctionResponse {
+            result: serde_json::to_value(response).map_err(FunctionError::by_serialization)?,
             ..Default::default()
         })
     }
@@ -120,7 +170,16 @@ impl Reminder {
 #[derive(Debug, Clone, Deserialize)]
 struct ReminderParameters {
     remind_at: Option<String>,
-    remind_in: Option<usize>,
+    remind_in: Option<i64>,
     cancel: Option<String>,
     content: String,
+}
+
+#[derive(Debug, Clone, Serialize)]
+#[serde(rename_all = "snake_case", tag = "status", content = "data")]
+enum ReminderResponse {
+    Registered { id: String, remind_at: String },
+    Cancelled { id: String },
+    DueLimitExceeded,
+    InvalidRequest,
 }
