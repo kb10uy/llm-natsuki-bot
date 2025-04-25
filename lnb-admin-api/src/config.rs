6737023dc8555337569ef96154c16801541ca42a
Yū KOBAYASHI <kb10uy@kb10uy.org>
2025-04-25 17:46:28 +0900

    lnb-admin-api を JWT 認証まで実装

---
+++ lnb-admin-api/src/config.rs
@@ -0,0 +1,22 @@
+use std::net::SocketAddr;
+
+use serde::Deserialize;
+use url::Url;
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct Config {
+    pub admin_api: ConfigAdminApi,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct ConfigAdminApi {
+    pub bind_address: SocketAddr,
+    pub auth: Option<ConfigAdminApiJwtAuth>,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct ConfigAdminApiJwtAuth {
+    pub jwt_header_name: String,
+    pub jwks_url: Url,
+    pub allowed_emails: Vec<String>,
+}
---
+++ lnb-admin-api/src/main.rs
@@ -0,0 +1,45 @@
+mod api;
+mod config;
+mod jwt_auth;
+
+use std::path::{Path, PathBuf};
+
+use anyhow::{Context, Result};
+use axum::Router;
+use clap::Parser;
+use tokio::{fs::read_to_string, net::TcpListener};
+
+#[derive(Debug, Clone, Parser)]
+#[clap(author, version)]
+struct Arguments {
+    #[clap(short, long, default_value = "./config.json")]
+    config: PathBuf,
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    tracing_subscriber::fmt::init();
+    let args = Arguments::parse();
+    let config = load_config(&args.config).await?;
+
+    let app = {
+        let mut router = routes();
+        if let Some(auth_config) = config.admin_api.auth {
+            router = router.layer(jwt_auth::JwtAuthLayer::new(auth_config)?);
+        }
+        router
+    };
+
+    let listener = TcpListener::bind(config.admin_api.bind_address).await?;
+    axum::serve(listener, app).await?;
+    Ok(())
+}
+
+fn routes() -> Router<()> {
+    Router::new().route("/test", todo!())
+}
+
+async fn load_config(path: impl AsRef<Path>) -> Result<config::Config> {
+    let config_str = read_to_string(path).await.context("failed to read config file")?;
+    serde_json::from_str(&config_str).context("failed to parse config")
+}
