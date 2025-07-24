c184b1b2a06e2d72928d486df560790c7bb9585c
Yū KOBAYASHI <kb10uy@kb10uy.org>
2025-07-24 14:46:33 +0900

    DateTimeProvider の定義を修正

--- lnb-core/src/interface/time.rs
+++ lnb-core/src/interface/time.rs
@@ -1,6 +1,8 @@
+use std::fmt::Debug;
+
 use time::OffsetDateTime;
 
 /// 日時情報の抽象化。
-pub trait DateTimeProvider {
+pub trait DateTimeProvider: Debug + Send + Sync {
     fn now(&self) -> OffsetDateTime;
 }
