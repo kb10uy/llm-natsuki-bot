a72c85769a02a31db7e7994534e10d0252433764
Yū KOBAYASHI <kb10uy@kb10uy.org>
2025-08-15 13:23:02 +0900

    InterpolatableTextProvider を実装

--- lnb-common/src/text_provider.rs
+++ lnb-common/src/text_provider.rs
@@ -1,4 +1,5 @@
 use lnb_core::interface::text::TextProvider;
+use upon::{Engine, Result as UponResult, Template, Value as UponValue};
 
 #[derive(Debug, Clone)]
 pub struct FixedTextProvider {
@@ -22,5 +23,27 @@ impl TextProvider for FixedTextProvider {
 impl From<String> for FixedTextProvider {
     fn from(value: String) -> Self {
         FixedTextProvider::new(value)
+    }
+}
+
+#[derive(Debug)]
+pub struct InterpolatableTextProvider {
+    engine: Engine<'static>,
+    template: Template<'static>,
+}
+
+impl InterpolatableTextProvider {
+    pub fn new(source: impl Into<String>) -> UponResult<InterpolatableTextProvider> {
+        let engine = Engine::new();
+        let template = engine.compile(source.into())?;
+        Ok(InterpolatableTextProvider { engine, template })
+    }
+}
+
+impl TextProvider for InterpolatableTextProvider {
+    type Data = UponValue;
+
+    fn generate(&self, data: Self::Data) -> String {
+        self.template.render(&self.engine, data).to_string().unwrap_or_default()
     }
 }
